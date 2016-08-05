3815b1c0456e9a1ff2145759713d684b17e43a53
Dominik Zieliński <dzielins42@gmail.com>
2016-08-05 23:31:45 +0200

    Add simple way to insert space between random strings in PatternNameGenerator. Without this, a single item array would be needed to add space between words.

--- src/main/java/pl/dzielins42/dmtools/generator/name/PatternNameGenerator.java
+++ src/main/java/pl/dzielins42/dmtools/generator/name/PatternNameGenerator.java
@@ -3,6 +3,8 @@ package pl.dzielins42.dmtools.generator.name;
 import pl.dzielins42.dmtools.generator.GeneratorOptions;
 
 public class PatternNameGenerator implements NameGenerator {
+    
+    public static final int SPACE = -1;
 
     private int[][] patterns;
     private String[][] arrays;
@@ -22,7 +24,11 @@ public class PatternNameGenerator implements NameGenerator {
         StringBuilder sb = new StringBuilder();
         // Iterate over pattern
         for (int i = 0; i < pattern.length; i++) {
-            sb.append(arrays[pattern[i]][options.nextInt(arrays[pattern[i]].length)]);
+            if (pattern[i] == SPACE) {
+                sb.append(" ");
+            } else {
+                sb.append(arrays[pattern[i]][options.nextInt(arrays[pattern[i]].length)]);
+            }
         }
 
         return sb.toString();
