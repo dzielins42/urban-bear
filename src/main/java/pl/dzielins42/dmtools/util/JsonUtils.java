975b85153527a0b35d41e7616ab1ab8cd33b4aec
Dominik Zieliński <dzielins42@gmail.com>
2016-11-08 21:22:01 +0100

    Expand JsonUtils to load multiple resources into single array.

--- src/main/java/pl/dzielins42/dmtools/util/JsonUtils.java
+++ src/main/java/pl/dzielins42/dmtools/util/JsonUtils.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import com.google.gson.Gson;
 
@@ -16,6 +19,35 @@ public class JsonUtils {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static String[] loadStringArrayFromFiles(String... jsonFilePaths) {
+        if (jsonFilePaths == null || jsonFilePaths.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        List<String> list = new LinkedList<String>();
+        String[] partial;
+        for (int i = 0; i < jsonFilePaths.length; i++) {
+            try {
+                partial = loadStringArrayFromFile(ResourcesUtils.getResourceFile(jsonFilePaths[i]));
+                if (partial != null) {
+                    list.addAll(Arrays.asList(partial));
+                }
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String[] result = null;
+        if (list != null && !list.isEmpty()) {
+            result = new String[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                result[i] = list.get(i);
+            }
+        }
+
+        return result;
     }
 
     public static String[] loadStringArrayFromFile(File jsonFile) {
@@ -42,4 +74,41 @@ public class JsonUtils {
 
         return null;
     }
+
+    public static String[] loadStringArrayFromFiles(File... jsonFiles) {
+        if (jsonFiles == null || jsonFiles.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        FileReader fr = null;
+        Gson gson = new Gson();
+        List<String> list = new LinkedList<String>();
+        for (int i = 0; i < jsonFiles.length; i++) {
+            try {
+                fr = new FileReader(jsonFiles[i]);
+                list.addAll(Arrays.asList(gson.fromJson(fr, String[].class)));
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (fr != null) {
+                    try {
+                        fr.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        String[] result = null;
+        if (list != null && !list.isEmpty()) {
+            result = new String[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                result[i] = list.get(i);
+            }
+        }
+
+        return result;
+    }
+
 }
